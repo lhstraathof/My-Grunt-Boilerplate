f561c575dd84fa3423ca2acefecb9ffdc2a3e2e2
Lars Straathof <lars@netsquare.nl>
2015-01-07 14:14:09 +0100

    watch for images changed

--- tasks/options/imagemin.js
+++ tasks/options/imagemin.js
@@ -1,10 +1,22 @@
 module.exports = {
-  dynamic: {
-    files: [{
-      expand: true,
-      cwd: 'assets/img/',
-      src: ['assets/img/**/*.{png,jpg,gif}'],
-      dest: 'img/'
-    }]
-  }
+  // dynamic: {
+  //   files: [{
+  //     expand: true,
+  //     cwd: 'assets/img/',
+  //     src: ['**/*.{png,jpg,gif}'],
+  //     dest: 'img/'
+  //   }]
+  // }
+  dynamic: {                         // Another target
+  	  options: {                       // Target options
+        // svgoPlugins: [{ removeViewBox: false }],
+        cache: false
+      },
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: '/assets/img/',            // Src matches are relative to Gruntfile path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'img/',                  // Destination path prefix
+      }]
+    }
 }
--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -3,24 +3,25 @@ module.exports = {
     livereload: true,
   },
   scripts: {
-    files: ['assets/js/*.js'],
+    files: ['assets/js/*.js', 'assets/js/vendor/*.js', 'assets/js/app/*.js'],
     tasks: ['jshint', 'concat', 'uglify'],
     options: {
       spawn: false,
     }
   },
   css: {
-    files: ['assets/css/*.scss'],
+    files: ['assets/css/*.scss', 'assets/css/**/*.scss'],
     tasks: ['sass', 'autoprefixer', 'cssmin'],
     options: {
       spawn: false,
     }
   },
   images: {
-    files: ['assets/images/**/*.{png,jpg,gif}', 'assets/images/*.{png,jpg,gif}'],
+    files: ['assets/img/**/*.{png,jpg,gif}', 'assets/img/*.{png,jpg,gif}'],
     tasks: ['imagemin'],
     options: {
       spawn: false,
+      event: ['changed', 'added', 'deleted'],
     }
   },
   html:{
