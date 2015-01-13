d17fe836358a16cfecc1610432dc3b3db010957c
Lars Straathof <lars@netsquare.nl>
2015-01-13 09:06:27 +0100

    weinre for mobile debugging

---
+++ tasks/options/weinre.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dev: {
+      options: {
+          httpPort: 8082,
+          boundHost: '10.0.0.207' // change this according to the server created by browserSync
+        }
+    }
+}
